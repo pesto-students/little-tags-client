cb2cad6a690cc023e92348e8c664cfb82e564fcf
bhk31 <bhkhandelwal92@gmail.com>
2021-02-26 01:50:41 +0530

    move css from app.css to carousel.css and create individual component in carousel

--- src/Carousel.js
+++ src/Carousel.js
@@ -1,29 +1,64 @@
-function Carousel(params) {
-  return(<>
-  <div class="slideshow-container">
-    <div class="mySlides fade">
-    <img class='slider-image' src="./1.jpg" />
-  </div>
+// import React from 'react';
+import { useState } from "react";
+import "./carousel.css";
 
-  <div class="mySlides fade">
-    <img class='slider-image' src="./2.jpg" />
-  </div>
-
-  <div class="mySlides fade">
-    <img class='slider-image' src="./3.jpg" />
-  </div>
-
-  <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
-  <a class="next" onclick="plusSlides(1)">&#10095;</a>
-</div>
-<br />
-
-<div >
-  <span class="dot" onclick="currentSlide(1)"></span>
-  <span class="dot" onclick="currentSlide(2)"></span>
-  <span class="dot" onclick="currentSlide(3)"></span>
-</div></>
+function Slide(params) {
+  return (
+    <div className="mySlides fade">
+      <img className="slider-image" src={params.src} alt={params.alt} />
+    </div>
   );
 }
+function Carousel(params) {
+  return (
+    <>
+      <div className="slideshow-container">
+        <Slide src="./1.jpg" alt="Slide 1"></Slide>
+        <Slide src="./2.jpg" alt="Slide 2"></Slide>
+        <Slide src="./3.jpg" alt="Slide 3"></Slide>
+
+        <a  className="prev" onClick={ShowSlides(-1)}>&#10094;</a>
+        <a  className="next" onClick={ShowSlides(1)}>&#10095;</a>
+      </div>
+      <br />
+
+      <div className="dot-container">
+        <span className="dot" onClick={ShowSlides(1)}></span>
+        <span className="dot" onClick={ShowSlides(2)}></span>
+        <span className="dot" onClick={ShowSlides(3)}></span>
+      </div>
+    </>
+  );
+
+  function ShowSlides(n) {
+
+    let [slideIndex, setSlideIndex] = useState(1);
+  // console.log(n);
+    if (slideIndex !== 1) {
+      setSlideIndex(n);
+    }
+  // console.log(slideIndex);
+    const slides = document.getElementsByClassName("mySlides");
+    // console.log(slides);
+    // const dots = document.getElementsByClassName("dot");
+  // console.log(dots);
+    // if (n > slides.length) {
+    //   setSlideIndex( 1 );
+    // }
+    // if (n < 1) {
+    //   setSlideIndex( slides.length);
+    // }
+    for (let i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    // for (let i = 0; i < dots.length; i++) {
+    //   dots[i].className = dots[i].className.replace(" active", "");
+    // }
+    slides[slideIndex - 1].style.display = "block";
+    // dots[slideIndex - 1].className += " active";
+  }
+}
+
+
 
 export { Carousel };
