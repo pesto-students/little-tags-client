02146797fa88de4ac4cd2ac32321a84703c50547
bhk31 <bhkhandelwal92@gmail.com>
2021-02-27 17:36:35 +0530

    move carousel component to component directory

--- src/component/carousel/Carousel.js
+++ src/component/carousel/Carousel.js
@@ -1,7 +1,7 @@
 // import React from 'react';
 import { useState } from "react";
 import "./carousel.css";
-import Button from '@material-ui/core/Button';
+
 function Carousel(params) {
   const slides = [
     {
