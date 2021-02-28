6a039baa9ed9eb0046a524e859b417566f5996d7
bhk31 <bhkhandelwal92@gmail.com>
2021-03-01 01:29:11 +0530

    add Category component and language changes for Carousel component

--- src/component/pages/HomePage.js
+++ src/component/pages/HomePage.js
@@ -1,9 +1,14 @@
 import React from "react";
 import { Carousel } from "../carousel/Carousel";
+import CategoryCard from "../category-card/CategoryCard";
 
 const HomePage = () => {
   return (
+    <>
       <Carousel></Carousel>
+      <CategoryCard></CategoryCard>
+      <br></br>
+      </>
   );
 };
 
