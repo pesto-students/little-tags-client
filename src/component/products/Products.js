ce405a3a272ffd710296354d2188e179ef416953
bhk31 <bhkhandelwal92@gmail.com>
2021-03-10 14:11:24 +0530

    add product page

--- src/component/products/Products.js
+++ src/component/products/Products.js
@@ -10,12 +10,12 @@ export default function Products(props) {
 
   return (
     <>
-      <Link exact="true" to={"#"} className="container">
+      <Link exact="true" to={"/product/1"} className="container">
         <div className="row" style={rowStyle}></div>
         <div className="product-title">{props.title}</div>
         <div>{props.subHeader}</div>
         <div className="price-container">
-          <span>Rs.{props.discountedPrice}</span> &nbsp;
+          <span className='discounted-price'>Rs.{props.discountedPrice}</span> &nbsp;
           <span>
             <del>Rs.{props.price}</del>
           </span>
