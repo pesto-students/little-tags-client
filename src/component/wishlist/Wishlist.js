e7cf194c788f0db862d02c154f0e81404307aba0
bhk31 <bhkhandelwal92@gmail.com>
2021-03-15 01:56:28 +0530

    add wishlist page

---
+++ src/component/wishlist/Wishlist.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./wishlist.css";
+import WishlistProductCard from "./WishlistProductCard";
+
+const Wishlist = () => {
+  return (
+    <div className="wishlist-container">
+      <WishlistProductCard />
+      <WishlistProductCard />
+      <WishlistProductCard />
+      <WishlistProductCard />
+      <WishlistProductCard />
+      <WishlistProductCard />
+      <WishlistProductCard />
+      <WishlistProductCard />
+      <WishlistProductCard />
+      <WishlistProductCard />
+    </div>
+  );
+};
+
+export default Wishlist;
