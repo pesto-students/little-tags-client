63014bea8ae1e2bd86a2111c83c056bb1a2eb517
bhk31 <bhkhandelwal92@gmail.com>
2021-03-15 19:53:52 +0530

    reduce width of product list page and add footer

---
+++ src/component/footer/Footer.js
@@ -0,0 +1,12 @@
+import "./footer.css";
+
+const Footer = () => {
+  return ( 
+    <center>
+      <p className="footer-para">Created By — Bhavesh Khandelwal & Aamir Alam at Pesto</p>
+    </center>
+  );
+};
+
+
+export default Footer;
